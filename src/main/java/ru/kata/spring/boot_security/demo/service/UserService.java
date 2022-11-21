7e386b8ddd53a1a1f35276e6ca2963c4da064d91
Stanislav Korolev <francet.stas@gmail.com>
2022-11-22 00:38:35 +0300

    added  username check for uniqueness

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,25 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.kata.spring.boot_security.demo.entity.User;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+
+    public List<User> getAllUsers();
+
+    void saveUser(User user);
+
+    User getUserById(Long id);
+
+    User getUserByUsername(String username);
+
+    void deleteUserById(Long id);
+
+    void updateUser(Long id, User updatedUser);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,93 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+//        user.setRoles(Collections.singleton(new Role("ROLE_USER"))); // TODO
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new UsernameNotFoundException(String.format("User with id '%s' not found!", id));
+        }
+    }
+
+    @Override
+    @Transactional
+    public User getUserByUsername(String username) {
+        return userRepository.getUserByUsername(username);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUserById(Long id) {
+        if (userRepository.findById(id).isPresent()) {
+            userRepository.deleteById(id);
+        } else throw new UsernameNotFoundException(String.format("User with id '%s' not found!", id));
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(Long id, User updatedUser) {
+        User userToUpdate = userRepository.getById(id);
+        userToUpdate.setUsername(updatedUser.getUsername());
+        userToUpdate.setPassword(updatedUser.getPassword());
+        userToUpdate.setConfirmPassword(updatedUser.getConfirmPassword());
+        userToUpdate.setFirstName(updatedUser.getFirstName());
+        userToUpdate.setLastName(updatedUser.getLastName());
+        userToUpdate.setAge(updatedUser.getAge());
+        userToUpdate.setEmail(updatedUser.getEmail());
+        userToUpdate.setRoles(updatedUser.getRoles());
+
+        userRepository.save(userToUpdate);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found!", username));
+        }
+        return user;
+    }
+
+
+}

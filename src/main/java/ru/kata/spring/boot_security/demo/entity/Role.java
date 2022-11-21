e43ddcece202365a2fc3a828fe695e55322bbd27
Stanislav Korolev <francet.stas@gmail.com>
2022-11-21 22:17:58 +0300

    upload my solution

---
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -0,0 +1,65 @@
+package ru.kata.spring.boot_security.demo.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/util/DBInit.java
@@ -0,0 +1,56 @@
+package ru.kata.spring.boot_security.demo.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.entity.Role;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class DBInit {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public DBInit(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @PostConstruct
+    public void initDB() {
+        Role roleAdmin = new Role(1L, "ROLE_ADMIN");
+        Role roleUser = new Role(2L, "ROLE_USER");
+        Set<Role> adminSet = new HashSet<>();
+        Set<Role> userSet = new HashSet<>();
+
+        roleService.addRole(roleAdmin);
+        roleService.addRole(roleUser);
+
+        adminSet.add(roleAdmin);
+        adminSet.add(roleUser);
+        userSet.add(roleUser);
+
+        User admin = new User("admin", "admin",
+                "Stepan", "Razin", 35,
+                "st.razin@mail.ru", adminSet);
+        admin.setId(1L);
+
+        User user = new User("user", "user",
+                "Emelyan", "Pugachev", 33,
+                "em.pugachev@mail.ru", userSet);
+
+        user.setId(2L);
+
+        userService.saveUser(admin);
+        userService.saveUser(user);
+    }
+
+
+}
